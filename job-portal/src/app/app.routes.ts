00d899dd553e35ccf7d16cb49644943ac17196f6
gergoEOQP48 <hegedusg2@gmail.com>
2025-11-06 15:20:22 +0100

    registration - frontend

--- job-portal/src/app/app.routes.ts
+++ job-portal/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './pages/registration/registration';
 
-export const routes: Routes = [];
+// export a routes változót
+export const routes: Routes = [
+  { path: 'register', component: RegistrationComponent },
+  { path: '', redirectTo: '/register', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
