cab34d7954ce492c2f040270ade242d77858a44a
Szilard <fabianszilard25@gmail.com>
2025-11-08 14:04:34 +0100

    fix: branching

--- job-portal/src/app/app.routes.ts
+++ job-portal/src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { RegistrationComponent } from './pages/registration/registration';
-import { JelentkezesiUrlapComponent } from './features/jelentkezes/jelentkezesi-urlap.component';
 
 // export a routes változót
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   { path: 'register', component: RegistrationComponent },
   { path: '', redirectTo: '/register', pathMatch: 'full' },
-  { path: 'jelentkezes', component: JelentkezesiUrlapComponent },
 ];
 
 @NgModule({
