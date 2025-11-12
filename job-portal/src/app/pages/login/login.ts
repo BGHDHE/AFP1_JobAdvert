f625bc79af64e69bb90970597cae33b12420a190
Rantott <benedekkerekes@gmail.com>
2025-11-12 23:36:18 +0100

    Renaming

--- job-portal/src/app/pages/login/login.ts
+++ job-portal/src/app/pages/login/login.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   imports: [],
-  templateUrl: './seeker_main.html',
-  styleUrl: './seeker_main.css',
+  templateUrl: './login.html',
+  styleUrls: ['./login.css'],
 })
 export class Login {
 
