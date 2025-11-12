30bd9d0511758525c2f076f167afd627562a87e7
Szilard <fabianszilard25@gmail.com>
2025-11-12 23:21:04 +0100

    feat: UI és routing

--- job-portal/src/app/app.routes.ts
+++ job-portal/src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
 import { RegistrationComponent } from './pages/registration/registration';
 
 // export a routes változót
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   { path: 'register', component: RegistrationComponent },
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
 ];
 
 @NgModule({
