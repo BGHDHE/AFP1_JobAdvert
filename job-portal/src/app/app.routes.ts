f68faf3f903d7cf9ef81fcba4aa6d0896ca65f22
Szilard <fabianszilard25@gmail.com>
2025-11-13 12:51:04 +0100

    feat: login routing

--- job-portal/src/app/app.routes.ts
+++ job-portal/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './pages/home/home';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RegistrationComponent } from './pages/registration/registration';
+import { Login } from './pages/login/login';
 
 // export a routes változót
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   { path: 'register', component: RegistrationComponent },
   { path: '', redirectTo: '/register', pathMatch: 'full' },
+  {path: 'login', component: Login}
 ];
 
 @NgModule({
