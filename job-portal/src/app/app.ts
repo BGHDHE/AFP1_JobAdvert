3aeea1e5c509bbbb97960c5ebccc23cf023f9986
Szilard <fabianszilard25@gmail.com>
2025-11-13 19:35:04 +0100

    feat: shared header

--- job-portal/src/app/app.ts
+++ job-portal/src/app/app.ts
@@ -2,6 +2,8 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routes';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
     RouterOutlet,
     ReactiveFormsModule,
     HttpClientModule,
+    Header
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
