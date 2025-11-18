5c72670fca2fca6b89f3c6307f3969e1a1370fab
Rantott <benedekkerekes@gmail.com>
2025-11-18 17:17:10 +0100

    Routing

--- job-portal/src/app/app.routes.ts
+++ job-portal/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { RegistrationComponent } from './pages/registration/registration';
 import { Login } from './pages/login/login';
 import { FelhasznaloprofilComponent } from './pages/felhasznaloprofil/felhasznaloprofil';
 import { SearchJobsComponent } from './pages/searchjobs/searchjobs';
+import { CompanyMain } from './pages/company-main/company-main';
+
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -14,7 +16,8 @@ export const routes: Routes = [
   {path: 'login', component: Login},
   { path: 'felhasznaloprofil', component: FelhasznaloprofilComponent},
   { path: 'felhasznaloprofil', component: FelhasznaloprofilComponent},
-  { path: 'searchjobs', component: SearchJobsComponent}
+  { path: 'searchjobs', component: SearchJobsComponent},
+   {path: 'company-main', component: CompanyMain}
 ];
 
 @NgModule({
