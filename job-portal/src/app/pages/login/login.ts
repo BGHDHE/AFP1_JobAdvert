e1194a81befd64e94675840f54402dbaa6cc9933
gergoEOQP48 <hegedusg2@gmail.com>
2025-11-27 13:15:23 +0100

    feature: login route

--- job-portal/src/app/pages/login/login.ts
+++ job-portal/src/app/pages/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.html',
+  imports: [CommonModule, FormsModule],
   styleUrls: ['./login.css'],
 })
 export class Login {
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.email === 'admin@example.com' && this.password === 'password123') {
+      this.router.navigate(['/company-main']); // átirányítás a CompanyMainComponent útvonalára
+    } else {
+      alert('Helytelen email vagy jelszó!');
+    }
+  }
 }
