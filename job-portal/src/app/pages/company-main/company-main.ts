dd2dc4ae3ae87921ba9341934bc975cc6f4ae2ed
Szilard <fabianszilard25@gmail.com>
2025-11-28 19:11:47 +0100

    feat: company-main frontend

--- job-portal/src/app/pages/company-main/company-main.ts
+++ job-portal/src/app/pages/company-main/company-main.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-company-main',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './company-main.html',
-  styleUrl: './company-main.css',
+  styleUrls: ['./company-main.css']
 })
-export class CompanyMain {
+export class CompanyMain implements OnInit {
+  jobs: any[] = [];
+  loading = false;
+  error: string = '';
 
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadJobs();
+  }
+
+  loadJobs(): void {
+    const user = this.authService.getUser();
+
+    if (!user || !user.id) {
+      this.error = 'Felhasználó nem bejelentkezve';
+      return;
+    }
+
+    this.loading = true;
+    this.http.get<any>(`http://localhost:3000/api/my-jobs/${user.id}`).subscribe({
+      next: (res) => {
+        this.jobs = res.jobs || [];
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
+        this.error = 'Hiba történt a hirdetések lekérésekor.';
+        console.error('API hiba:', err);
+      }
+    });
+  }
+
+  createNewJob(): void {
+    alert('coming soon');
+  }
+
+  editJob(jobId: number): void {
+    alert('coming soon');
+
+  }
+
+  deleteJob(jobId: number): void { 
+    alert('coming soon')
+    }
 }
